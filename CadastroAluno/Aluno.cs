f34620ae2e2940445abf5d33a13a8db3acad61ec
maisa-ed <maisaeduarda080@gmail.com>
2021-11-22 21:36:51 -0300

    logica de cadastro

--- CadastroAluno/Aluno.cs
+++ CadastroAluno/Aluno.cs
@@ -6,46 +6,105 @@ namespace CadastroAluno
     {
         string[] pesquisa;
         string[] nomes = new string[10];
-        int[] idade = new int[10];
+        int[] idades = new int[10];
         int posicao, valor, quantidade = 0;
+        int opcao = 0;
         
         public void CadastrarAluno()
         {
-            var opcao = Menu();
-
-            switch (opcao)
+    
+            while (opcao != 5)
             {
-                case 1:
+                Console.Clear();
+                opcao = Menu();
+                switch (opcao)
                 {
-                    Console.WriteLine("teste1");
-                    //Menu();
+                    case 1:
+                    {
+                        Console.WriteLine("Quantos usuários serão cadastrados? Máximo de 10 usuários");
 
-                    break;
-                }
+                        quantidade = Convert.ToInt32(Console.ReadLine());
 
-                case 2:
-                {
-                    break;
-                }
+                        for (int posicao = 1; posicao <= quantidade; posicao++)
+                        {
+                            Console.WriteLine("Qual o nome do usuário?");
+                            var nome = Console.ReadLine();
+                            nomes[posicao - 1] = nome;
 
-                case 3:
-                {
-                    break;
-                }
+                            Console.WriteLine("Qual a idade do usuário?");
+                            var idade = Convert.ToInt32(Console.ReadLine());
+                            idades[posicao - 1] = idade;
 
-                case 4:
-                {
-                    break;
-                }
+                        }
 
-                case 5:
-                {
-                    break;
+                        Console.WriteLine("Usuário(s) cadastrado(s) com sucesso!");
+                        break;
+                    }
+
+                    case 2:
+                    {
+                        Console.WriteLine("Lista de usuários cadastrados:\n");
+
+                        for (int posicao = 1; posicao <= quantidade; posicao++)
+                        {
+                            if (idades[posicao-1]!= 0)
+                            {
+                                Console.WriteLine($"Os usuários encontrados foram: {nomes[posicao - 1]} - {idades[posicao - 1]} anos");
+                                
+                            }
+                        }
+                        break;
+                    }
+
+                    case 3:
+                    {
+                        Console.WriteLine("Qual usuário você deseja buscar?");
+                        Console.WriteLine("Informe o nome do usuário");
+                        
+                        var nome = Console.ReadLine();
+
+                        for (int posicao = 1; posicao <= quantidade; posicao++)
+                        {
+                            if (nomes[posicao - 1] == nome)
+                            {
+                                Console.WriteLine($"Nome do aluno(a) encontrado: {nomes[posicao - 1]} - {idades[posicao - 1]} anos");
+                            }
+                            
+                        }
+
+                        break;
+                    }
+
+                    case 4:
+                    {
+                        Console.WriteLine("Informe o usuário a ser excluído");
+                        
+                        var nome = Console.ReadLine();
+                        
+                        for (int posicao = 1; posicao <= quantidade; posicao++)
+                        {
+                            if (nomes[posicao - 1] == nome)
+                            {
+                                nomes[posicao-1] = nomes[posicao + 1];
+                                idades[posicao-1] = idades[posicao + 1];
+                            }
+
+                        }
+                        
+                        Console.WriteLine("Usuário deletado com sucesso!");
+                    }
+                        break;
+
+                    case 5:
+                    {
+                        Console.WriteLine("Cadastro de alunos encerrado. Muito obrigado!");
+                    }
+                        break;
                 }
             }
         }
 
-        public int Menu()
+        private int Menu()
         {
             Console.WriteLine("**************************************\n");
             Console.WriteLine("CADASTRO DE USUÁRIOS\n");
